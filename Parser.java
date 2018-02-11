e085a3fb88ef7f2ae92d64d4742856f77f62a36f
Sabian98 <32489090+Sabian98@users.noreply.github.com>
2018-02-11 14:53:42 +0600

    updated

--- Parser.java
+++ Parser.java
@@ -3,64 +3,203 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package parse_app;
 
-/**
- *
- * @author sabian98
- */
-public class Parse_app {
+
+public class MainActivity extends Activity {
+
+    private final String mainActivityTag = "MainActivity";
+    private JSONObject jObject;
+    private SignInDbAdapter mSignInDbHelper;
+    private ProgressDialog mProgressDialog;
+    String loginmessage = null;
+    Thread t;
+    private SharedPreferences mPreferences;
+    Button signin;
 
     /**
-     * @param args the command line arguments
+     * Called when the activity is first created.
      */
-    SharedPreferences mPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+        mSignInDbHelper = new SignInDbAdapter(this);
+        mSignInDbHelper.open();
 
-    if (!checkLoginInfo() 
-        ) {
-   login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  String urlAddArray;
-                StringRequest request = new StringRequest(Request.Method.POST, urlLoginArray, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        if (!response.trim.equals("fail")) {
-                            authToken = String.valueOf(response.get("auth_token"));
-                            //  System.out.println(response.toString());
-                            // SharedPreferences preferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
-                            mPreferences.edit().putString("token", authToken).commit();
-                            editor.putString("UserName", name2.getText().toString());
-                            editor.putString("PassWord", pw.getText().toString());
-                            editor.commit();
-                        } else {
-                            //show error messgae
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+        if (!checkLoginInfo()) {
+            signin = (Button) findViewById(R.id.btn_sign_in);
+            signin.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                    showDialog(0);
+                    t = new Thread() {
+                        public void run() {
+                            try {
+                                authenticate();
+                            } catch (ClientProtocolException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }//end onResponse
-                }, new Response.ErrorListener() {
+                    };
+                    t.start();
+                }
+            });
+        } else {
+            /*
+			 * Directly opens the Welcome page, if the username and password is
+			 * already available in the SharedPreferences
+             */
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> parameters = new HashMap<>();
-                        parameters.put("name", name2.getText().toString());
-                        parameters.put("password", pw.getText().toString());
-                        //  parameters.put("date",date.getText().toString());
-
-                        return parameters;
-                    }
-                };
-                requestQueue.add(request);
-            }
-
-        });
+            // Trying to minimize the number of screens
+            Intent intent = new Intent(this, MainMenuActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 0: {
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Please wait while signing in ...");
+                mProgressDialog.setIndeterminate(true);
+                mProgressDialog.setCancelable(true);
+                return mProgressDialog;
+            }
+        }
+        return null;
+    }
+
+    private void authenticate() throws ClientProtocolException, IOException {
+        try {
+            String pin = "";
+            HashMap<String, String> sessionTokens = signIn();
+
+        } catch (Exception e) {
+            Intent intent = new Intent(getApplicationContext(), LoginError.class);
+            intent.putExtra("LoginMessage", "Unable to login");
+            startActivity(intent);
+            removeDialog(0);
+        }
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            String loginmsg = (String) msg.obj;
+            if (loginmsg.equals("SUCCESS")) {
+                removeDialog(0);
+                Intent intent = new Intent(getApplicationContext(), WorkoutDetailsListActivity.class);
+                //Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+
+    private HashMap<String, String> signIn() {
+
+        HashMap<String, String> sessionTokens = null;
+
+        EditText mEmailField = (EditText) findViewById(R.id.email_field);
+        EditText mPasswordField = (EditText) findViewById(R.id.password_field);
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://yoursite.com/sessions");
+        JSONObject holder = new JSONObject();
+        JSONObject userObj = new JSONObject();
+
+        try {
+            userObj.put("password", password);
+            userObj.put("email", email);
+            holder.put("user", userObj);
+            StringEntity se = new StringEntity(holder.toString());
+            post.setEntity(se);
+            post.setHeader("Accept", "application/json");
+            post.setHeader("Content-Type", "application/json");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JSONException js) {
+            js.printStackTrace();
+        }
+
+        String response = null;
+        try {
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            response = client.execute(post, responseHandler);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ParsedLoginDataSet parsedLoginDataSet = new ParsedLoginDataSet();
+        //ParsedLoginDataSet parsedLoginDataSet = myLoginHandler.getParsedLoginData();
+        try {
+
+            /*if (response == null) {
+				System.out.println("response is null " + response);
+				Exception e = new Exception();
+				throw e;
+			}*/
+            sessionTokens = parseToken(response);
+            mSignInDbHelper.createSession(mEmailField.getText().toString(),
+                    sessionTokens.get("auth_token"));
+            // now = Long.valueOf(System.currentTimeMillis());
+            // mSignInDbHelper.createSession(mEmailField.getText().toString(),mAuthToken,now);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        parsedLoginDataSet.setExtractedString(sessionTokens.get("error"));
+        if (parsedLoginDataSet.getExtractedString().equals("Success")) {
+            // Store the username and password in SharedPreferences after the successful login
+            SharedPreferences.Editor editor = mPreferences.edit();
+            editor.putString("UserName", email);
+            editor.putString("PassWord", password);
+            editor.commit();
+            Message myMessage = new Message();
+            myMessage.obj = "SUCCESS";
+           handler.sendMessage(myMessage);
+        } else {
+            Intent intent = new Intent(getApplicationContext(), LoginError.class);
+            intent.putExtra("LoginMessage", "Invalid Login");
+            startActivity(intent);
+            removeDialog(0);
+        }
+
+        return sessionTokens;
+
+    }
+
+    public HashMap<String, String> parseToken(String jsonResponse)
+            throws Exception {
+        HashMap<String, String> sessionTokens = new HashMap<String, String>();
+        if (!jsonResponse.trim(). equals("fail")) {
+            jObject = new JSONObject(jsonResponse);
+            JSONObject sessionObject = jObject.getJSONObject("output");
+            String attributeError = sessionObject.getString("error");
+            String attributeToken = sessionObject.getString("auth_token");
+           // String attributeConsumerKey = sessionObject.getString("consumer_key");
+           // String attributeConsumerSecret = sessionObject
+                   // .getString("consumer_secret");
+            sessionTokens.put("error", attributeError);
+            sessionTokens.put("auth_token", attributeToken);
+            //sessionTokens.put("consumer_key", attributeConsumerKey);
+           // sessionTokens.put("consumer_secret", attributeConsumerSecret);
+        } else {
+            sessionTokens.put("error", "Error");
+        }
+        return sessionTokens;
+    }
+
+    // Checking whether the username and password has stored already or not
     private final boolean checkLoginInfo() {
         boolean username_set = mPreferences.contains("UserName");
         boolean password_set = mPreferences.contains("PassWord");
@@ -70,4 +209,4 @@ public class Parse_app {
         return false;
     }
 
-}//end app
+}
